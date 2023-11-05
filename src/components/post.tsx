7389896d64ebfbd6de71f19092b32d7b3a888527
ygkim <ygkim527@naver.com>
2023-11-05 23:18:41 +0900

    vh vs dvh 차이점 정리

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -8,5 +8,12 @@ import { z } from 'zod';
 import { Range } from 'react-date-range';
 
 export default function Post() {
-  return <article>Post Content</article>;
+  /* 
+  보통 화면의 높이를 설정할 때 vh를 사용한다
+  vh는 viewport height로 스크린 크기에 맞게 height가 결정된다.
+  하지만 모바일 브라우저에 따라 viewport가 다르기 때문에 화면이 짤리는 경우가 있다 (ex. safari)
+  vh 종류로는 svh (Short Viewport Height),  lvh (Large Viewport Height), dvh (Dynamic Viewport Height)가 있다.
+  해당 브라우저의 viewport에 맞게 설정해 주려면 dvh를 사용해주면 된다. (너비인 vw도 동일하게 사용 가능)
+  */
+  return <article className='h-[100dvh]'>Post Content</article>;
 }
