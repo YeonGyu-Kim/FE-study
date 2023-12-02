3021a7fce9de9094cb7a5412c609090c2ab03246
ygkim <ygkim527@naver.com>
2023-12-02 23:36:21 +0900

    shadcn으로 Form 및 Calendar 사용하기

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -21,6 +21,13 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Calendar, CalendarProps } from './ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import { Calendar as CalendarIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import { ko } from 'date-fns/esm/locale';
+import { useState } from 'react';
 
 const signUpSchema = z
   .object({
@@ -31,6 +38,26 @@ const signUpSchema = z
   .refine((data) => data.password === data.confirmPassword, {
     message: '비밀번호가 같아야 합니다.',
     path: ['confrimPassword'],
+  });
+
+const formSchema = z
+  .object({
+    username: z.string(),
+    email: z.string().email(),
+    password: z.string().min(5, '비밀번호는 최소 5자리 입니다.'),
+    confirmPassword: z.string(),
+    dates: z.date({
+      required_error: '날짜를 선택하세요',
+      description: '설명',
+    }),
+    /*     dates: z.object({
+      from: z.date(),
+      to: z.date(),
+    }), */
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: '비밀번호를 확인하세요.',
+    path: ['confirmPassword'],
   });
 
 type SignupSchema = z.infer<typeof signUpSchema>;
@@ -44,6 +71,12 @@ export default function Post() {
   해당 브라우저의 viewport에 맞게 설정해 주려면 dvh를 사용해주면 된다. (너비인 vw도 동일하게 사용 가능)
   */
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   const {
     register,
     handleSubmit,
@@ -55,12 +88,27 @@ export default function Post() {
     resolver: zodResolver(signUpSchema),
   });
 
-  const onSubmit = (data: unknown) => {
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log(data);
   };
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      dates: undefined,
+      /*  dates: {
+        from: undefined,
+        to: undefined,
+      }, */
+    },
+  });
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    /*  <form onSubmit={handleSubmit(onSubmit)}>
       <input {...register('email')} type='email' placeholder='Email' />
       <input {...register('password')} type='password' placeholder='Password' />
       {errors.password && <div>{errors.password.message}</div>}
@@ -70,6 +118,144 @@ export default function Post() {
         placeholder='ConfirmPassword'
       />
       <button type='submit'>제출</button>
-    </form>
+    </form> */
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2'>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='이름' {...field} type='text' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder='이메일' {...field} type='email' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder='비밀번호' {...field} type='password' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='confirmPassword'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ConfirmPassword</FormLabel>
+              <FormControl>
+                <Input placeholder='비밀번호 확인' {...field} type='password' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='dates'
+          render={({ field }) => (
+            <FormItem>
+              <FormMessage />
+              <Popover open={isOpen} onOpenChange={toggleOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={'outline'}
+                    className={cn(
+                      'w-[280px] justify-start text-left font-normal',
+                      !field.value && 'text-muted-foreground'
+                    )}
+                  >
+                    <CalendarIcon className='mr-2 h-4 w-4' />
+                    {field.value ? (
+                      format(field.value, 'yyyy-MM-dd', { locale: ko })
+                    ) : (
+                      <span>날짜 선택</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className='w-auto p-0'>
+                  <Calendar
+                    mode='single'
+                    defaultMonth={field.value}
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    onDayClick={toggleOpen}
+                    locale={ko}
+                  />
+                </PopoverContent>
+              </Popover>
+            </FormItem>
+          )}
+        />
+        {/*  <FormField
+          control={form.control}
+          name='dates'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ConfirmPassword</FormLabel>
+              <FormMessage />
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={'outline'}
+                    className={cn(
+                      'w-[280px] justify-start text-left font-normal',
+                      !field.value.from && 'text-muted-foreground'
+                    )}
+                  >
+                    <CalendarIcon className='mr-2 h-4 w-4' />
+                    {field.value.from ? (
+                      field.value?.to ? (
+                        <>
+                          {format(field.value?.from, 'LLL dd, y')}{' '}
+                          {format(field.value?.to, 'LLL dd, y')}
+                        </>
+                      ) : (
+                        format(field.value?.from, 'LLL dd, y')
+                      )
+                    ) : (
+                      <span>Select your dates</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className='w-auto p-0'>
+                  <Calendar
+                    mode='range'
+                    defaultMonth={field.value.from}
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </FormItem>
+          )}
+        /> */}
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
   );
 }
