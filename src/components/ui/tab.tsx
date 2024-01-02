46636c4160a291490f8f91c281d221feeb87933f
ygkim <ygkim527@naver.com>
2024-01-03 01:02:15 +0900

    Tab Ui Vertical 예시

--- src/components/ui/tab.tsx
+++ src/components/ui/tab.tsx
@@ -13,13 +13,15 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function TabsDemo() {
   return (
-    <Tabs defaultValue='account' className='w-[400px]'>
-      <TabsList className='grid w-full grid-cols-2'>
-        <TabsTrigger value='account'>Account</TabsTrigger>
-        <TabsTrigger value='password'>Password</TabsTrigger>
+    /* <Tabs defaultValue='account' className='w-[400px]'>
+      <TabsList className='rounded-b-none border border-b-0 p-0 h-full'>
+        <TabsTrigger value='account'>도면 1</TabsTrigger>
+        <TabsTrigger value='password'>도면 2</TabsTrigger>
+        <TabsTrigger value='account2'>도면 3</TabsTrigger>
+        <TabsTrigger value='password2'>도면 4</TabsTrigger>
       </TabsList>
       <TabsContent value='account'>
-        <Card>
+        <Card className='rounded-tl-none shadow-none'>
           <CardHeader>
             <CardTitle>Account</CardTitle>
             <CardDescription>
@@ -56,6 +58,106 @@ export default function TabsDemo() {
             </div>
             <div className='space-y-1'>
               <Label htmlFor='new'>New password</Label>
+              <Input id='new' type='password' />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs> */
+    <Tabs defaultValue='account' className='flex'>
+      <TabsList className='flex flex-col h-full rounded-tr-none'>
+        <TabsTrigger value='account'>도면 1</TabsTrigger>
+        <TabsTrigger value='password'>도면 2</TabsTrigger>
+        <TabsTrigger value='account2'>도면 3</TabsTrigger>
+        <TabsTrigger value='password2'>도면 4</TabsTrigger>
+      </TabsList>
+      <TabsContent value='account'>
+        <Card className='rounded-tl-none bg-muted'>
+          <CardHeader>
+            <CardTitle>도면1</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, youll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className='space-y-2'>
+            <div className='space-y-1'>
+              <Label htmlFor='current'>Current password</Label>
+              <Input id='current' type='password' />
+            </div>
+            <div className='space-y-1'>
+              <Label htmlFor='new'>New password2</Label>
+              <Input id='new' type='password' />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value='password'>
+        <Card>
+          <CardHeader>
+            <CardTitle>도면2</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, youll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className='space-y-2'>
+            <div className='space-y-1'>
+              <Label htmlFor='current'>Current password</Label>
+              <Input id='current' type='password' />
+            </div>
+            <div className='space-y-1'>
+              <Label htmlFor='new'>New password</Label>
+              <Input id='new' type='password' />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value='account2'>
+        <Card>
+          <CardHeader>
+            <CardTitle>도면3</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, youll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className='space-y-2'>
+            <div className='space-y-1'>
+              <Label htmlFor='current'>Current password</Label>
+              <Input id='current' type='password' />
+            </div>
+            <div className='space-y-1'>
+              <Label htmlFor='new'>New password</Label>
+              <Input id='new' type='password' />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value='password2'>
+        <Card>
+          <CardHeader>
+            <CardTitle>도면4</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, youll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className='space-y-2'>
+            <div className='space-y-1'>
+              <Label htmlFor='current'>Current password</Label>
+              <Input id='current' type='password' />
+            </div>
+            <div className='space-y-1'>
+              <Label htmlFor='new'>New password2</Label>
               <Input id='new' type='password' />
             </div>
           </CardContent>
